b282fb11209ee2e39ed65472e83ab0c83a23b8cf
2319367578 <503573491@qq.com>
2016-06-13 21:02:17 +0800

    张冰 两个dal

---
+++ Storage.DAL/ExportProductDal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Storage.Model;
+namespace Storage.DAL
+{
+    public class ExportProductDal:BaseDal<ExportProduct>
+    {
+        public int Add(ExportProduct export)
+        {
+            return base.Add("Insert into ExportProduct values(@proid,@count,@exporttime,@price,@totalmoney,@cusid,@userid)",export);
+        }
+        public int Delete(ExportProduct export)
+        {
+            return base.Delete("Update ExportProduct set IsDel=@isdel where id=@id",export);
+        }
+        public int Update(ExportProduct export)
+        {
+            return base.Update("Update ExportProduct set Proid=@proid,Count=@count,ExportTime=@exporttime,Price=@price,TotalMoney=@totalmoney,Cusid=@cusid,UserId=@userid where Id=@id",export);
+        }
+        public IEnumerable<ExportProduct> GetSignleEntities()
+        {
+            return base.GetSignleEntities("select * from ExportProduct where IsDel=0",null);
+        }
+    }
+}
---
+++ Storage.DAL/IncludeProductDal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Storage.Model;
+namespace Storage.DAL
+{
+    public class IncludeProductDal:BaseDal<IncludeProduct>
+    {
+        public int Add(IncludeProduct include)
+        {
+            return base.Add("insert into IncludeProduct values(@proid,@count,@price,@cusid,@includeTime,@userid)", include);
+        }
+        public int Delete(IncludeProduct include)
+        {
+            return base.Update("update IncludeProduct set IsDel=@isdel where id=@id",include);
+        }
+        public int Update(IncludeProduct include)
+        {
+            return base.Update("update IncludeProduct set ProId=@proid,Count=@count,Price=@price,CusId=@cusid,IncludeTime=@includeTime,UserId=@userid where id=@id",include);
+        }
+        public IEnumerable<IncludeProduct> GetSignleEntities()
+        {
+            return base.GetSignleEntities("select * from IncludeProduct where isDel = 0", null);
+        }
+    }
+}
